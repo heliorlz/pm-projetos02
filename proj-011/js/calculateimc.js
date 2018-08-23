a5b8c2581debd09173823304d4b0f31353e3b0bb
Helio Alves <heliorlz@helio.alves>
2018-08-23 07:31:42 -0300

    iniciando js

---
+++ proj-011/js/calculateimc.js
@@ -0,0 +1,12 @@
+function showResult() {
+    document.querySelector(".result").classList.remove("hidden")
+    document.querySelector(".btn").classList.add("hidden")
+
+}
+
+function showBtnImc() {
+    document.querySelector(".result").classList.add("hidden")
+    document.querySelector(".btn").classList.remove("hidden")
+
+}
+
