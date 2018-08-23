0070f22258fc5d325aad9ba6a2c6c07e16f128e8
Helio Alves <heliorlz@helio.alves>
2018-08-23 07:29:14 -0300

    iniciando js

--- proj-001/js/calculateimc.js
+++ proj-001/js/calculateimc.js
@@ -1,3 +1,12 @@
-function calculateImc() {
-    alert('Hello World JS')
+function showResult() {
+    document.querySelector(".result").classList.remove("hidden")
+    document.querySelector(".btn").classList.add("hidden")
+
 }
+
+function showBtnImc() {
+    document.querySelector(".result").classList.add("hidden")
+    document.querySelector(".btn").classList.remove("hidden")
+
+}
+
