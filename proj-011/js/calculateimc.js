4183fe35e717b240954bead1ada58c36bcf96f95
Helio Alves <heliorlz@helio.alves>
2018-08-31 07:47:26 -0300

    validação simples dos inputs

--- proj-011/js/calculateimc.js
+++ proj-011/js/calculateimc.js
@@ -1,6 +1,9 @@
 // as funções show Result e showBtnImc servem para adicionar ou remover a class .hidden, para alternar a exibição,
 // ora botão para calculo imc, ora resultado
 
+// Harlley utiliza a função $(elemento) {return document.querySelector(elemento)}, assim podemos substituir toda a expressão
+// document.querySelector() por $()
+
 // variaveis dentro de funções são locais, não podendo ser utilizadas fora de sua função
 
 function showResult() {
@@ -13,20 +16,37 @@ function showBtnImc() {
     document.querySelector(".div-btn").classList.remove("hidden")
 }
 
-// funções que, através do evento click do botão, realiza certas tarefas 
-document.querySelector('.btn-imc').addEventListener('click', function(){
-    calculateResult()
-    showResult()
+// funções que, através do evento click do botão, realiza certas tarefas
+// Harlley ensinou refatorar o cédigo, deixar ele mais clean,como por exemplo, trocar a function que é anonima por => (arrow)
+document.querySelector('.btn-imc').addEventListener('click', function(){ 
+    let height = document.querySelector('#height')
+    let weight = document.querySelector('#weight')
+    
+    //  condicional para validar os inputs 
+    if (height.value == "") {
+        alert('Digite sua altura, respeitando o formato convencional (ex: 1,70)!')
+        height.focus()
+    }
+    else if (weight.value == "") {
+        alert('Digite seu peso em kg!')
+        weight.focus()
+    }
+    else {
+        // foi necessário colocar as funções dentro do if, demorei a entender que era preciso seguir a ordem corretamente
+        calculateResult(height, weight)
+        showResult()   
+    }
 })
 
 document.querySelector('.back').addEventListener('click', function(){
     showBtnImc()
 })
 
+
 // função que mostra o resultado e a descrição, chamada através do click do .btn-imc 
-function calculateResult() {
+function calculateResult(height, weight) {
     // const imc calculamos, de fato, o imc do usuário, e retornamos na interface através do innerHTML 
-    const imc = calculateIMC(document.querySelector('#height').value, document.querySelector('#weight').value)
+    const imc = calculateIMC(height.value, weight.value)
     // através do toLocaleString, para formatarmos a saída da melhor forma 
     document.querySelector('.imc-value').innerHTML = imc.toLocaleString('pt-BR', {maximumFractionDigits: 2})
     document.querySelector('.imc-desc').innerHTML = descriptionIMC(imc)
